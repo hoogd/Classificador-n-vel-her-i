11386993023b3ae702596505657887f72dcfc5f1
hoogd <hoogd8020@gmail.com>
2023-12-25 10:36:32 -0300

    classnivelheroi

---
+++ projeto.js
@@ -0,0 +1,36 @@
+// variável de nível
+let xp = 9000
+// variável jogador
+let jogador = "CL7"
+// tipos de nívels
+if (xp <= 1000){
+    let f = "Ferro"
+            console.log(" O Herói de nome " + jogador, "está no nível de " + f )
+} if(xp >= 1001  && xp <= 2000){
+    let b = "Bronze"
+            console.log(" O Herói de nome " + jogador, "está no nível de " + b );
+    }
+     if (xp >= 2001 && xp <= 5000){
+        let p = "Prata"
+                console.log(" O Herói de nome " + jogador, "está no nível de " + p )
+    }
+    if(xp >= 5001 && xp <= 7000){
+        let o = "Ouro"
+                console.log(" O Herói de nome " + jogador, "está no nível de " + o )
+    }
+     if (xp >= 7001 && xp <= 8000){
+        let pl = "Platina"
+                console.log(" O Herói de nome " + jogador, "está no nível de " + pl )
+    }
+      if (xp >= 8001 && xp <= 9000){
+        let as = "Ascendente"
+                console.log(" O Herói de nome " + jogador, "está no nível de " + as )
+    }
+    if (xp >= 9001 && xp <= 10000){
+        let i = "Imortal"
+                console.log(" O Herói de nome " + jogador, "está no nível de " + i )
+    }
+    if (xp >= 10001){
+        let rad = "Radiante"
+                console.log(" O Herói de nome " + jogador, "está no nível de " + rad )
+    }
